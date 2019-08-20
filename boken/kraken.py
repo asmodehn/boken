d3e796aada4fa490e2191ce191a3667ee5178805
AlexV <asmodehn@gmail.com>
2019-08-20 18:28:06 +0200

    adding first basic implementation with test and cassette.

---
+++ boken/kraken.py
@@ -0,0 +1,17 @@
+from uplink import Consumer, get, headers, returns
+
+
+@headers({
+    "User-Agent": "Uplink-Sample-App"
+})
+class Kraken(Consumer):
+
+    @returns.json
+    @get("0/public/Time")
+    def get_time(self):
+        """return the time"""
+
+
+k = Kraken(base_url="https://api.kraken.com/")
+
+print(k.get_time())
---
+++ tests/test_kraken.py
@@ -0,0 +1,17 @@
+import pytest
+
+from boken.kraken import k
+
+
+@pytest.mark.vcr()
+def test_time():
+    response = k.get_time()
+    print(response)
+    # asserting structure (but marshmallow could do it)
+    assert not response.get('error')
+    assert response.get('result').get('rfc1123')
+    assert response.get('result').get('unixtime')
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', __file__])
